337388d0f49c0752d549cb6a74703da4031d2c4a
leo oliveira <leoigornunes@protonmail.com>
2025-01-25 10:25:16 -0300

    change test upload_s3_parquet_file to consider dataset=True

--- test/test_utils.py
+++ test/test_utils.py
@@ -13,18 +13,19 @@ def s3_bucket():
 def test_upload_s3_parquet_file_integration(s3_bucket):
     df = pd.DataFrame({"col1": [1, 2, 3], "col2": ["a", "b", "c"]})
 
-    path = f"s3://{s3_bucket}/test-folder/data.parquet"
+    path = f"s3://{s3_bucket}/test-folder/"
     
     upload_s3_parquet_file(path, df)
     s3_client= boto3.client('s3')
     try:
-        s3_client.head_object(Bucket=s3_bucket,Key="test-folder/data.parquet")
+        response = s3_client.list_objects_v2(Bucket=s3_bucket,Prefix="test-folder/")
+        assert 'Contents' in response and len(response['Contents']) > 0, f"No files found at {path}"
     except s3_client.exceptions.ClientError:
         assert False, f"File not found at {path}"
 
-    s3_client.delete_object(Bucket=s3_bucket, Key="test-folder/data.parquet")
-    try:
-        s3_client.head_object(Bucket=s3_bucket, Key="test-folder/data.parquet")
-        assert False, f"File not deleted at {path}"
-    except s3_client.exceptions.ClientError:
-        pass
+
+    for obj in response.get('Contents', []):
+        s3_client.delete_object(Bucket=s3_bucket, Key=obj['Key'])
+    
+    response = s3_client.list_objects_v2(Bucket=s3_bucket, Prefix="test-folder/")
+    assert 'Contents' not in response or len(response['Contents']) == 0, f"Files not deleted at {path}"
