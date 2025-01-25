9939a928a41e55f34b0731eded8edb31cea0221b
leo oliveira <leoigornunes@protonmail.com>
2025-01-25 02:58:14 -0300

    initial commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from gasto-deputados!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_version = ">=1.10.3"
+    backend "s3" {
+        bucket = "terraform-states-9723"
+        key = "gastos_deputados/state.tfstate"
+        region = "us-east-2"
+      
+    }
+    
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.82"
+        }
+    }
+
+}
+provider "aws" {
+    region = "us-east-2"
+}
