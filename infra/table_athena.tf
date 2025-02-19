9927f6c77b54ad8e861e7772d47801b07229ae5a
leo oliveira <leoigornunes@protonmail.com>
2025-02-19 20:29:10 -0300

    feat: change location of the gastos table

--- infra/table_athena.tf
+++ infra/table_athena.tf
@@ -83,7 +83,7 @@ resource "aws_glue_catalog_table" "gastos" {
     }
 
     storage_descriptor {
-        location      = "s3://${aws_s3_bucket.main_bucket-dev.bucket}/gastos/"
+        location      = "s3://${aws_s3_bucket.main_bucket-dev.bucket}/processed/gastos/"
         input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
         output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
 
